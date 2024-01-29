f9cc28ca7914218a49686b211026c1b9abaa0876
beel2x <155056945+beel2x@users.noreply.github.com>
2024-01-28 20:31:10 -0600

    Add files via upload

---
+++ 1_parameter_VS_arguments.js
@@ -0,0 +1,10 @@
+//Function parameters are the names listed in the function's definition. 
+//Function arguments are the real values passed to the function.
+// Parameters are initialized to the values of the arguments supplied.
+
+
+
+function add(a,b){  //a and b are Parameter
+    console.log(a+b)
+}
+add(10,20)//10 20 are arguments
